24715e5a32f9e52701406937d5727a20d040a206
Matthias Stute <134229065+MatthiasStu@users.noreply.github.com>
2024-05-06 17:39:36 +0200

    bugs included

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HeaderComponent } from './shared/header/header.component';
 import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { AboutMeSectionComponent } from "./about-me-section/about-me-section.component";
@@ -8,14 +12,35 @@ import { PortfolioComponent } from "./portfolio/portfolio.component";
 import { ContactMeComponent } from "./contact-me/contact-me.component";
 import { FooterComponent } from './footer/footer.component';
 
-
-@Component({
+@AppComponent({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     imports: [RouterOutlet, HeaderComponent, LandingPageComponent, AboutMeSectionComponent, MySkillsComponent, PortfolioComponent, ContactMeComponent, FooterComponent]
 })
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 export class AppComponent {
   title = 'matthias-stute-portfolio';
 }
+
+@NgModule({
+
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([]), 
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
