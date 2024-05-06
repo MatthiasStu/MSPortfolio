b0f5a9637148131db9f3b7bc4cb6a71fc1feb103
Matthias Stute <134229065+MatthiasStu@users.noreply.github.com>
2024-05-06 13:55:15 +0200

    deleted wrong imports

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,23 +1,17 @@
 import { Component } from '@angular/core';
-import { TranslateService} from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-landing-page',
+  standalone : true, 
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss'] // ändere styleUrl zu styleUrls
+  styleUrls: ['./landing-page.component.scss'] 
 })
 export class LandingPageComponent {
   mailadress:string = 'matthiasstute@yahoo.de';
   linkedin:string = 'https://www.linkedin.com/in/matthias-stute-664147305/';
   github:string = 'https://github.com/MatthiasStu';
 
-  constructor(private translate: TranslateService){ 
-    translate.setDefaultLang('en');
-  }
-
-  switchLanguage(language: string) {
-    this.translate.use(language);
-  }
 
   redirectToExternalWebsite(x:number){
     if(x == 2){
